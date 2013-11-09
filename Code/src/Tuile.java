065e847b8234a712d25562427a4cc435e8e80228
DelalandeNicolas <ni.delalande@laposte.net>
2013-11-09 22:40:10 +0100

    Ajout des fonctions pour repérer les abbayes

--- Code/src/Tuile.java
+++ Code/src/Tuile.java
@@ -60,7 +60,7 @@ public class Tuile {
 	}
 	
 	public boolean verifPoseTuileLegale (ArrayList<Tuile> adjacente, int sens){
-		// dans le ArrayList<Tuile> l'ordre correspond à leur place par rapport à la tuile qu'on veut poser : 0 = dessus, 1 = droite, 2 = en dessus, 3 = à gauche
+		// dans l'attribut ArrayList<Tuile> tuileAdjacentes, l'ordre correspond à leur place par rapport à la tuile qu'on veut poser : 0 = dessus, 1 = droite, 2 = dessous, 3 = à gauche
 		if(  (this.tabCarac[0]==adjacente.get(0).tabCarac[2] || adjacente.get(0)==null)
 				&& (this.tabCarac[1]==adjacente.get(1).tabCarac[3] || adjacente.get(1)==null)
 				&& (this.tabCarac[2]==adjacente.get(2).tabCarac[0] || adjacente.get(2)==null) 
@@ -71,6 +71,110 @@ public class Tuile {
 		else return false;
 	}
 
+	
+	
+	public ArrayList<Tuile> verifPresenceAbbaye (){
+		// Fonctions à appliquer sur la tuile qui vient d'être posé et aprés que le pion a été placé ( ou pas )
+		//Vérifie si il y a une abbaye sur la tuile et/ou sur les tuile autour ( adjacente et diagonale ) et l'ajoute dans un ArrayList si c'est le cas
+		//Rappel : dans l'attribut ArrayList<Tuile> tuileAdjacentes, l'ordre correspond à leur place par rapport à la tuile qu'on vient de poser : 0 = dessus, 1 = droite, 2 = dessous, 3 = à gauche
+		
+		
+		ArrayList<Tuile> TuileAbbaye = new ArrayList<Tuile>();
+		
+		if(this.tabCarac[4]==Terrain.ABBAYE){ //pour la tuile qui vient d'être posé 
+			TuileAbbaye.add(this);
+		}
+		if(this.tuileAdjacentes.get(0)!=null){ // pour la tuile du dessus
+			if((this.tuileAdjacentes.get(0)).tabCarac[4]==Terrain.ABBAYE){
+				TuileAbbaye.add(this.tuileAdjacentes.get(0));
+			}
+			
+		}
+		if(this.tuileAdjacentes.get(1)!=null){ // pour la tuile de droite
+			if((this.tuileAdjacentes.get(1)).tabCarac[4]==Terrain.ABBAYE){
+				TuileAbbaye.add(this.tuileAdjacentes.get(1));
+			}
+			
+		}
+		if(this.tuileAdjacentes.get(2)!=null){ // pour la tuile du dessous
+			if((this.tuileAdjacentes.get(2)).tabCarac[4]==Terrain.ABBAYE){
+				TuileAbbaye.add(this.tuileAdjacentes.get(2));
+			}
+			
+		}
+		if(this.tuileAdjacentes.get(3)!=null){ // pour la tuile de gauche
+			if((this.tuileAdjacentes.get(3)).tabCarac[4]==Terrain.ABBAYE){
+				TuileAbbaye.add(this.tuileAdjacentes.get(3));
+			}
+			
+		}
+		
+		// les 8 if suivant vérifient la presence d'abbaye sur les diagonales (2 if par diagonale car 2 chemin pour y accéder), mais ne peut détecter une abbaye qui est en haut à gauche de la tuile posé si celle-ci n'a
+		// ni tuile au-dessus et à gauche d'elle. Mais pas de probléme car dans ce cas l'abbaye est forcément imcompléte et donc rien à faire.
+		
+		if( this.tuileAdjacentes.get(0)!=null && this.tuileAdjacentes.get(0).tuileAdjacentes.get(3)!=null){ // diagonale haut/gauche en passant par le haut
+			if (this.tuileAdjacentes.get(0).tuileAdjacentes.get(3).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(0).tuileAdjacentes.get(3));
+			}
+		}	
+		else if (this.tuileAdjacentes.get(3)!=null && this.tuileAdjacentes.get(3).tuileAdjacentes.get(0)!=null) { // else pour éviter de l'ajouter 2 fois si les 2 chemins sont valide, diagonale haut/gauche en passant par la gauche
+			if (this.tuileAdjacentes.get(3).tuileAdjacentes.get(0).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(3).tuileAdjacentes.get(0));
+			}
+		}
+		
+		if( this.tuileAdjacentes.get(0)!=null && this.tuileAdjacentes.get(0).tuileAdjacentes.get(1)!=null){ // diagonale haut/droite en passant par le haut
+			if (this.tuileAdjacentes.get(0).tuileAdjacentes.get(1).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(0).tuileAdjacentes.get(1));
+			}
+		}	
+		else if (this.tuileAdjacentes.get(1)!=null && this.tuileAdjacentes.get(1).tuileAdjacentes.get(0)!=null) { // diagonale haut/droite en passant par la droite
+			if (this.tuileAdjacentes.get(1).tuileAdjacentes.get(0).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(1).tuileAdjacentes.get(0));
+			}
+		}
+		
+		if( this.tuileAdjacentes.get(2)!=null && this.tuileAdjacentes.get(2).tuileAdjacentes.get(1)!=null){ // diagonale bas/droite en passant par le bas
+			if (this.tuileAdjacentes.get(2).tuileAdjacentes.get(1).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(2).tuileAdjacentes.get(1));
+			}
+		}	
+		else if (this.tuileAdjacentes.get(1)!=null && this.tuileAdjacentes.get(1).tuileAdjacentes.get(2)!=null) { // diagonale bas/droite en passant par la droite
+			if (this.tuileAdjacentes.get(1).tuileAdjacentes.get(2).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(1).tuileAdjacentes.get(2));
+			}
+		}
+		
+		if( this.tuileAdjacentes.get(2)!=null && this.tuileAdjacentes.get(2).tuileAdjacentes.get(3)!=null){ // diagonale bas/gauche en passant par le bas
+			if (this.tuileAdjacentes.get(2).tuileAdjacentes.get(3).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(2).tuileAdjacentes.get(3));
+			}
+		}	
+		else if (this.tuileAdjacentes.get(3)!=null && this.tuileAdjacentes.get(3).tuileAdjacentes.get(2)!=null) { // diagonale bas/gauche en passant par la gauche
+			if (this.tuileAdjacentes.get(3).tuileAdjacentes.get(2).tabCarac[4]==Terrain.ABBAYE) {
+				TuileAbbaye.add(this.tuileAdjacentes.get(3).tuileAdjacentes.get(2));
+			}
+		}
+		
+		return TuileAbbaye;
+				
+	}
+	
+	
+	public boolean verifAbbayeComplete (){
+		// vérifie si une abbaye est compléte
+		if ( this.tuileAdjacentes.get(0)!=null && this.tuileAdjacentes.get(1)!=null && this.tuileAdjacentes.get(2)!=null && this.tuileAdjacentes.get(3)!=null ) { // les 4 adjacentes
+				return (this.tuileAdjacentes.get(0).tuileAdjacentes.get(3)!=null && this.tuileAdjacentes.get(0).tuileAdjacentes.get(1)!=null && // les 2 diagonales du haut
+						this.tuileAdjacentes.get(2).tuileAdjacentes.get(3)!=null && this.tuileAdjacentes.get(2).tuileAdjacentes.get(1)!=null ); // les 2 diagonales du bas
+		}
+		else return false;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
