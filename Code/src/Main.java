a62f22b61081dc69e74e5063a328c53a21b06259
DelalandeNicolas <ni.delalande@laposte.net>
2013-11-16 21:28:38 +0100

    Modif pour poser Tuile + test

--- Code/src/Main.java
+++ Code/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main {
 
@@ -6,7 +9,65 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println((-1)%4);
+		Pioche p = new Pioche();
+		Plateau r = new Plateau(150,75);
+		Tuile t1 = new Tuile(
+				new Terrain [] {Terrain.VILLE, Terrain.ROUTE, Terrain.CHAMPS, Terrain.ROUTE, Terrain.ROUTE}, // Caractéristiques des bords.
+				new boolean [] {false, false, true, true, true, true, true, true}, // Présence des champs.
+				new boolean [][] { // Tableau de connexité des caractéristiques.
+						{false, false, false, false, false}, 
+						{false, false, false, true , true }, 
+						{false, false, false, false, false}, 
+						{false, true , false, false, true }, 
+						{false, true , false, true , false}},
+				new boolean [][] { // Tableau de connexité des champs.
+						{false, false, false, false, false, false, false, true },
+						{false, false, false, false, false, false, false, false},
+						{false, false, false, false, false, false, false, false},
+						{false, false, false, false, true , true , true , false},
+						{false, false, false, true , false, true , true , false},
+						{false, false, false, true , true , false, true , false},
+						{false, false, false, true , true , true , false, false},
+						{false, false, true , false, false, false, false, false}},
+				5);
+		ArrayList<Tuile> adj = new ArrayList<Tuile>();
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		adj = r.getTuileAdjacentes(x, y);
+		if(t1.verifPoseTuileLegale(adj,0)){		
+			t1.poseTuile(adj, r, x, y);
+			System.out.println("tuile1 posé");
+		}
+		
+		Tuile t2 = new Tuile(new Terrain [] {Terrain.ROUTE, Terrain.ROUTE, Terrain.ROUTE, Terrain.ROUTE, Terrain.ROUTE}, // Caractéristiques des bords.
+				new boolean [] {true, true, true, true, true, true, true, true}, // Présence des champs.
+				new boolean [][] { // Tableau de connexité des caractéristiques.
+						{false, false, false, false, false}, 
+						{false, false, false, false, false}, 
+						{false, false, false, false, false}, 
+						{false, false, false, false, false}, 
+						{false, false, false, false, false}},
+				new boolean [][] { // Tableau de connexité des champs.
+						{false, false, false, false, false, false, false, true },
+						{false, false, true , false, false, false, false, false},
+						{false, true , false, false, false, false, false, false},
+						{false, false, false, false, true , false, false, false},
+						{false, false, false, true , false, false, false, false},
+						{false, false, false, false, false, false, true , false},
+						{false, false, false, false, false, true , false, false},
+						{true , false, false, false, false, false, false, false}},
+				5);
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		adj = r.getTuileAdjacentes(x, y);
+		if(t2.verifPoseTuileLegale(adj,0)){		
+			t2.poseTuile(adj, r, x, y);
+			System.out.println("tuile2 posé");
+		}
+		
 	}
 
 }
--- Code/src/Tuile.java
+++ Code/src/Tuile.java
@@ -23,7 +23,19 @@ public class Tuile {
 		this.tabConnexitéBordure=connexiteBordure;
 		this.tabConnexitéChamps=connexiteChamps;
 		this.bouclier=bouclier;
+		this.tuileAdjacentes=new ArrayList<Tuile>(4);
 		
+	}
+	
+	public Tuile (){
+		this.tabCarac= new Terrain[4];
+		for(int i =0;i<4;i++){
+		this.tabCarac[i]=Terrain.VIDE;
+		}
+		this.tabPresenceChamps= new boolean [1];
+		this.tabConnexitéBordure= new boolean [1][1];
+		this.tabConnexitéChamps=new boolean [1][1];
+		this.bouclier=5;
 	}
 	
 	public void retirePion(){
@@ -62,10 +74,10 @@ public class Tuile {
 	
 	public boolean verifPoseTuileLegale (ArrayList<Tuile> adjacente, int sens){
 		// dans l'attribut ArrayList<Tuile> tuileAdjacentes, l'ordre correspond à leur place par rapport à la tuile qu'on veut poser : 0 = dessus, 1 = droite, 2 = dessous, 3 = à gauche
-		if(  (this.tabCarac[0]==adjacente.get(0).tabCarac[2] || adjacente.get(0)==null)
-				&& (this.tabCarac[1]==adjacente.get(1).tabCarac[3] || adjacente.get(1)==null)
-				&& (this.tabCarac[2]==adjacente.get(2).tabCarac[0] || adjacente.get(2)==null) 
-				&& (this.tabCarac[3]==adjacente.get(3).tabCarac[1] || adjacente.get(3)==null) ){
+		if(  (this.tabCarac[0]==adjacente.get(0).tabCarac[2] || adjacente.get(0).tabCarac[0]==Terrain.VIDE)
+				&& (this.tabCarac[1]==adjacente.get(1).tabCarac[3] || adjacente.get(1).tabCarac[0]==Terrain.VIDE)
+				&& (this.tabCarac[2]==adjacente.get(2).tabCarac[0] || adjacente.get(2).tabCarac[0]==Terrain.VIDE) 
+				&& (this.tabCarac[3]==adjacente.get(3).tabCarac[1] || adjacente.get(3).tabCarac[0]==Terrain.VIDE) ){
 		
 		return true;
 		}
@@ -75,10 +87,18 @@ public class Tuile {
 	public void poseTuile (ArrayList<Tuile> adjacente, Plateau r, int x, int y){
 		// pré-requis : la pose de la tuile est légale
 		r.setTuile(this, x, y);
-		if (adjacente.get(0)!=null) {this.tuileAdjacentes.add(0,adjacente.get(0));}
-		if (adjacente.get(1)!=null) {this.tuileAdjacentes.add(1,adjacente.get(1));}
-		if (adjacente.get(2)!=null) {this.tuileAdjacentes.add(2,adjacente.get(2));}
-		if (adjacente.get(3)!=null) {this.tuileAdjacentes.add(3,adjacente.get(3));}
+		
+		this.tuileAdjacentes.add(0,adjacente.get(0));
+		if (adjacente.get(0).tabCarac[0]!=Terrain.VIDE) {this.tuileAdjacentes.get(0).tuileAdjacentes.add(2,this);} // ajoute this dans tuileAdjacente de la tuile du dessus si ce n'est pas une Tuile vide 
+		
+		this.tuileAdjacentes.add(1,adjacente.get(1));
+		if (adjacente.get(1).tabCarac[0]!=Terrain.VIDE) {this.tuileAdjacentes.get(1).tuileAdjacentes.add(3,this);}
+		
+		this.tuileAdjacentes.add(2,adjacente.get(2));
+		if (adjacente.get(2).tabCarac[0]!=Terrain.VIDE) {this.tuileAdjacentes.get(2).tuileAdjacentes.add(0,this);}
+		
+		this.tuileAdjacentes.add(3,adjacente.get(3));
+		if (adjacente.get(3).tabCarac[0]!=Terrain.VIDE) {this.tuileAdjacentes.get(3).tuileAdjacentes.add(1,this);}
 	}
 	
 	
@@ -179,7 +199,22 @@ public class Tuile {
 		else return false;
 	}
 	
-	
+	public int evalAbbayeFinPartie(Plateau r){
+		// pré-requis : tuile avec un pion, en fin de partie
+		// action : retourne la valeur d'une abbaye en fin de partie
+		int res=1; 
+		if ( r.getTuile(this.x-1,this.y+1)!=null ){ res++;}
+		if ( r.getTuile(this.x,this.y+1)!=null ){ res++;}
+		if ( r.getTuile(this.x+1,this.y+1)!=null ){ res++;}
+		if ( r.getTuile(this.x+1,this.y)!=null ){ res++;}
+		
+		if ( r.getTuile(this.x+1,this.y-1)!=null ){ res++;}
+		if ( r.getTuile(this.x,this.y-1)!=null ){ res++;}
+		if ( r.getTuile(this.x-1,this.y-1)!=null ){ res++;}
+		if ( r.getTuile(this.x-1,this.y)!=null ){ res++;}
+		
+		return res;
+	}
 	
 	
 	
