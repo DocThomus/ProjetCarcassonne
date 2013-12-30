7715b98afce85edbab8361991a2ef54127d53b08
DocThomus <bonninthom2@gmail.com>
2013-12-30 16:07:57 +0100

    Ajout du bouton passer à l'historique

--- InterfaceGraphique/InterfaceHistorique/VueHistorique.java
+++ InterfaceGraphique/InterfaceHistorique/VueHistorique.java
@@ -8,7 +8,9 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,8 +20,10 @@ import Principal.ContPrincipal;
 public class VueHistorique implements Observer {
 	private static final int LARGEUR_HISTORIQUE = (int) (ContPrincipal.LARGEUR_FENETRE*0.1);
 	private static final int HAUTEUR_HISTORIQUE = ContPrincipal.HAUTEUR_FENETRE;
+	private static final int INTERLIGNE_HISTORIQUE = 2;
 	
 	private ArrayList<JLabel> etiquettesMessages;
+	private JButton boutonPasser;
 
 	public VueHistorique(JFrame fenetrePrincipale, GridBagConstraints contraintesLayout) {
 		JPanel panHistorique = new JPanel();
@@ -29,15 +33,20 @@ public class VueHistorique implements Observer {
 		
 		
 		panHistorique.setLayout(new BoxLayout(panHistorique, BoxLayout.Y_AXIS));
-		panHistorique.setBorder(BorderFactory.createEmptyBorder(5, 10, 0, 0));
+		panHistorique.setBorder(BorderFactory.createEmptyBorder(5, 10, 10, 0));
 		panHistorique.add(new JLabel("Historique :"));
 		
 		etiquettesMessages = new ArrayList<JLabel>();
 		for(int i = 0; i < 10; i++) {
+			panHistorique.add(Box.createVerticalStrut(INTERLIGNE_HISTORIQUE));
 			JLabel e = new JLabel();
 			panHistorique.add(e);
 			etiquettesMessages.add(e);
 		}
+		
+		boutonPasser = new JButton("Passer");
+		panHistorique.add(Box.createGlue());
+		panHistorique.add(boutonPasser);
 		
 		panHistorique.setPreferredSize(new Dimension(LARGEUR_HISTORIQUE, HAUTEUR_HISTORIQUE));
 		panHistorique.setMinimumSize(new Dimension(LARGEUR_HISTORIQUE, HAUTEUR_HISTORIQUE));
