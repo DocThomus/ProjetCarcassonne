f873499454e061a34fa53fafba9c948dd24d0b42
DelalandeNicolas <ni.delalande@laposte.net>
2014-01-04 10:49:09 +0100

    BoutonPlateau

---
+++ InterfaceGraphique/InterfacePlateau/BoutonPlateau.java
@@ -0,0 +1,25 @@
+package InterfacePlateau;
+
+import javax.swing.JButton;
+
+public class BoutonPlateau extends JButton {
+	
+	private String nom;
+	private int x;
+	private int y;	
+	
+	public BoutonPlateau (String nom ,int x , int y){
+		super(nom);
+		this.x=x;
+		this.y=y;
+	}
+	
+	public int getX (){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+}
